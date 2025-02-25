b293abe7012ff733a9ec5a9fd0b72627a9c78b56
Kim Thomasrud <kima90@hotmail.no>
2025-02-25 19:29:18 +0100

    Ryddet opp i redundans og kode

--- B25ITK39_Unity/Assets/Scripts/Player.cs
+++ B25ITK39_Unity/Assets/Scripts/Player.cs
@@ -4,8 +4,6 @@ public class Player : MonoBehaviour {
 
     public GameObject bus;
     public GameObject sportsCar;
-    public GameObject busVirtualCamera;
-    public GameObject sportsCarVirtualCamera;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,13 +11,11 @@ public class Player : MonoBehaviour {
         if (PlayerData.PD.carChoice != 0)
         {
             sportsCar.SetActive(false);
-            busVirtualCamera.SetActive(true);
         }
 
         if (PlayerData.PD.carChoice != 1)
         {
             bus.SetActive(false);
-            sportsCarVirtualCamera.SetActive(true);
         }
     }
 
