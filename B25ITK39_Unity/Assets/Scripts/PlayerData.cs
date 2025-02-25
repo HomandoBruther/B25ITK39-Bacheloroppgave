8370744e833d99cbc8c1876d21dc259913b51014
Kim Thomasrud <kima90@hotmail.no>
2025-02-25 22:00:06 +0100

    Lagde litt funksjoner som kan brukes for å registrere passasjerer og score i PlayerData.

--- B25ITK39_Unity/Assets/Scripts/PlayerData.cs
+++ B25ITK39_Unity/Assets/Scripts/PlayerData.cs
@@ -2,17 +2,29 @@ using UnityEngine;
 
 public class PlayerData : MonoBehaviour
 {
+    
+    
+    public static PlayerData PD;
+
+    //Car Stats
     public int carChoice;
     public int health;
-    public static PlayerData PD;
+    public int currentPassengers;
+    public int currentImportantPassengers;
+    public int maxPassengers;
+
+    //Player Stats
+    public string name;
+    public int points;
+    public int money;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        MakeThisTheOnlyGameManager();
+        MakeThisTheOnlyPlayerData();
     }
 
-    void MakeThisTheOnlyGameManager()
+    void MakeThisTheOnlyPlayerData()
     {
     https://discussions.unity.com/t/accessing-variables-on-a-script-on-a-dontdestroyonload-gameobject/149506
         if (PD == null)
@@ -27,5 +39,60 @@ public class PlayerData : MonoBehaviour
                 Destroy(gameObject);
             }
         }
+    }
+
+    public int FillPassengers(int passengers)
+    {
+        if (maxPassengers >= currentPassengers + passengers + currentImportantPassengers)
+        {
+            currentPassengers += passengers;
+            return 0;
+        }
+        else
+        {
+            currentPassengers = maxPassengers;
+            return (currentPassengers + passengers) - maxPassengers;
+        }  
+    }
+
+    public int FillImportantPassengers(int passengers)
+    {
+        if (maxPassengers >= currentPassengers + passengers + currentImportantPassengers)
+        {
+            currentPassengers += passengers;
+            return 0;
+        }
+        else
+        {
+            currentPassengers = maxPassengers;
+            return (currentPassengers + passengers) - maxPassengers;
+        }
+    }
+
+    int EmptyPassengers()
+    {
+        int emptiedPassengers = currentPassengers;
+        currentPassengers = 0;
+
+        return emptiedPassengers;
+    }
+
+    int EmptyImportantPassengers()
+    {
+        int emptiedImporantPassengers = currentImportantPassengers;
+        currentImportantPassengers = 0;
+
+        return emptiedImporantPassengers;
+    }
+
+    int scorePoints()
+    {
+        int currentScoring = 0;
+        currentScoring += EmptyPassengers() * 100;
+        currentScoring += EmptyImportantPassengers() * 1000;
+
+        points += currentScoring;
+
+        return currentScoring;
     }
 }
