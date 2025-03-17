db37fa5306167817a52ad878fc6777fc5aee8d1b
HomandoBruther <76850754+HomandoBruther@users.noreply.github.com>
2025-03-17 13:30:32 +0100

    Added possibility of controlling bus while in air

--- B25ITK39_Unity/Assets/Scripts/AirControl.cs
+++ B25ITK39_Unity/Assets/Scripts/AirControl.cs
@@ -3,18 +3,23 @@ using UnityEngine;
 
 public class AirControl : MonoBehaviour
 {
-
     [Header("Dash Settings")]
     [SerializeField] private float dashForce = 5000f;
     [SerializeField] private float dashDuration = 0.2f;
     [SerializeField] private float dashCooldown = 2f;
+    [Header("Air Control Settings")]
+    [SerializeField] private float airRotationSpeed = 50f;
+    [SerializeField] private float groundCheckDistance = 1.5f;
+    [SerializeField] private LayerMask groundLayer;
+
     private bool canJump = true;
     private bool isInAir = false;
-
     private Rigidbody rb;
 
+    private float horizontalInput, verticalInput;
+    SceneHandler sceneHandler;
+    GameObject theGameController;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -22,26 +27,30 @@ public class AirControl : MonoBehaviour
         sceneHandler = theGameController.GetComponent<SceneHandler>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Check ground status
+        CheckGrounded();
+
+        // Handle jump (dash) input
         if (Input.GetKeyDown(KeyCode.Space) && canJump)
         {
             StartCoroutine(Jump());
         }
+
+        // Update speed in scene handler
         sceneHandler.speed = Mathf.FloorToInt(rb.linearVelocity.magnitude);
     }
-
-    private float horizontalInput, verticalInput;
-    private float currentSteerAngle, currentBreakForce;
-
-    SceneHandler sceneHandler;
-    GameObject theGameController;
-
 
     private void FixedUpdate()
     {
         GetInput();
+
+        // Apply air control if in the air
+        if (isInAir)
+        {
+            AirSteering();
+        }
     }
 
     private void GetInput()
@@ -50,53 +59,41 @@ public class AirControl : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
     }
 
-
     private IEnumerator Jump()
     {
         canJump = false;
 
         // Get input direction
-        Vector3 inputDirection = transform.up * verticalInput + transform.right * horizontalInput;
+        Vector3 jumpUp = transform.up;
 
-        // If no input, default to forward
+        /*
+        Vector3 inputDirection = transform.up * verticalInput + transform.right * horizontalInput;
         if (inputDirection == Vector3.zero)
             inputDirection = transform.forward;
-
         inputDirection.Normalize();
+        */
 
-        // Apply dash force in the calculated direction
-        rb.AddForce(inputDirection * dashForce, ForceMode.Impulse);
+        // Apply dash force
+        rb.AddForce(jumpUp * dashForce, ForceMode.Impulse);
         yield return new WaitForSeconds(dashDuration);
         yield return new WaitForSeconds(dashCooldown);
         canJump = true;
     }
 
-
-    private void HandleAerialSteering()
+    private void CheckGrounded()
     {
-        /*
-        currentSteerAngle = maxSteerAngle * horizontalInput;
-        frontLeftWheelCollider.steerAngle = currentSteerAngle;
-        frontRightWheelCollider.steerAngle = currentSteerAngle;
-        */
+        isInAir = !Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundLayer);
     }
 
-    private void UpdateWheels()
+    private void AirSteering()
     {
-        /*
-        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
-        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
-        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
-        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
-        */
+        float pitch = verticalInput * -airRotationSpeed * Time.fixedDeltaTime;
+        float yaw = horizontalInput * airRotationSpeed * Time.fixedDeltaTime;
+        float roll = 0f;
+
+        if (Input.GetKey(KeyCode.Q)) roll = airRotationSpeed * Time.fixedDeltaTime;
+        if (Input.GetKey(KeyCode.E)) roll = -airRotationSpeed * Time.fixedDeltaTime;
+
+        transform.Rotate(pitch, yaw, roll, Space.Self);
     }
-
-    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
-    {
-        wheelCollider.GetWorldPose(out Vector3 pos, out Quaternion rot);
-        wheelTransform.position = pos;
-        wheelTransform.rotation = rot;
-    }
-
-
 }
