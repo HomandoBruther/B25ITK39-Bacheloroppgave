e970f51d432dbba34af0ad7131cde11eb8452303
Wolff404 <stavrosw@hotmail.com>
2025-03-19 23:11:48 +0100

    Engine sound now respects car speed

--- B25ITK39_Unity/Assets/Scripts/EngineSound.cs
+++ B25ITK39_Unity/Assets/Scripts/EngineSound.cs
@@ -4,25 +4,46 @@ public class EngineSound : MonoBehaviour
 {
     
     AudioSource audioSource;
+    Rigidbody rigidBody;
 
+
+
+    public float minSpeed = 0f;
+    public float maxSpeed = 20f;
     public float minimumPitch = 0.5f;
     public float maximumPitch = 2.5f;
 
-    public float engineSpeed = 0.5f;
+    //public float engineSpeed = 0.5f;
 
 
 
     void Start()
     {
-
+        rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
         audioSource.pitch = minimumPitch;
         
+        if (audioSource == null)
+        {
+            Debug.LogError("No AudioSource found on " + gameObject.name);
+        }
+        if (rigidBody == null)
+        {
+            Debug.LogError("No Rigidbody found on " + gameObject.name);
+        }
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        if (rigidBody != null)
+        {
+            float speed = rigidBody.linearVelocity.magnitude; // Get bus speed
+            float normalizedSpeed = Mathf.InverseLerp(minSpeed, maxSpeed, speed);
+            audioSource.pitch = Mathf.Lerp(minimumPitch, maximumPitch, normalizedSpeed);
+        }
+
+        /*
         audioSource.pitch = engineSpeed;
 
         if(engineSpeed < minimumPitch) {
@@ -32,5 +53,6 @@ public class EngineSound : MonoBehaviour
         }else{
             audioSource.pitch = engineSpeed;
         }
+        */
     }
 }
