91d23396478b36ade5c74c7c6d524b4de31b0269
Wolff404 <stavrosw@hotmail.com>
2025-03-19 16:56:32 +0100

    Added temporary music and engine sound

---
+++ B25ITK39_Unity/Assets/Scripts/AudioSlider.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class AudioSlider : MonoBehaviour
+{
+   
+
+
+// Script going to be used for audio 
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
