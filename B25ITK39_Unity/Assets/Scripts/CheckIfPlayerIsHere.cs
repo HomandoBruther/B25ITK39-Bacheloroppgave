a4f377b2f97ea82347be8bb2542d374466859b9d
HomandoBruther <76850754+HomandoBruther@users.noreply.github.com>
2025-04-03 11:44:02 +0200

    Added Henrik scripts

---
+++ B25ITK39_Unity/Assets/Scripts/CheckIfPlayerIsHere.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckIfPlayerIsHere : MonoBehaviour
+{
+    PlayerRespawnHandler respawnHandler;
+    public Transform checkPoint;
+
+    // Get the component from the bus
+    private void Awake()
+    {
+        respawnHandler = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerRespawnHandler>();
+    }
+
+    // When player enters the trigger set a new location for respawn
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            respawnHandler.UpdateCheckPoint(checkPoint);
+        }
+    }
+
+}
